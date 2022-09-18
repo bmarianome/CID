aedd1019a1c121869dbd3faa195a4a006962fb99
Mariano Benitez <benitezmarianito@gmail.com>
2022-09-18 10:19:21 -0300

    Probando plausible

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const { withPlausibleProxy } = require('next-plausible')
 
-module.exports = withPlausibleProxy()({
+module.exports = withPlausibleProxy({
+    customDomain: 'https://plausible.bmariano.me',
+})({
     reactStrictMode: true,
     swcMinify: true,
 })
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,25 +8,25 @@ function Cid({ Component, pageProps }: AppProps) {
     
     return (
         <>
-            <Head>
-                <link rel="shortcut icon" type="image/png" href="/favicon.png" />
-                <link rel="apple-touch-icon" type='image/png' href="/favicon.png" />
-                <meta name="theme-color" content="#EF6A34" />
-
-                <meta content="Centro de Imágenes" property="og:title" />
-                <meta property="og:description" content="CID, primer servicio de imágenes médicas totalmente digital de las Sierras Chicas." />
-                <meta content="https://www.cidvillaallende.com.ar/favicon.png" property="og:image" />
-                <meta content="https://www.cidvillaallende.com.ar/" property="og:url" />
-                <meta content="website" property="og:type" />
-
-                <link rel="manifest" href="/manifest.json" />
-            </Head> 
-            
             <PlausibleProvider 
                 customDomain='https://plausible.bmariano.me' 
                 domain="cidvillaallende.com.ar" 
-                trackOutboundLinks={true} selfHosted={true}>
+                trackOutboundLinks={true} selfHosted={true}
+            >
 
+                <Head>
+                    <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+                    <link rel="apple-touch-icon" type='image/png' href="/favicon.png" />
+                    <meta name="theme-color" content="#EF6A34" />
+
+                    <meta content="Centro de Imágenes" property="og:title" />
+                    <meta property="og:description" content="CID, primer servicio de imágenes médicas totalmente digital de las Sierras Chicas." />
+                    <meta content="https://www.cidvillaallende.com.ar/favicon.png" property="og:image" />
+                    <meta content="https://www.cidvillaallende.com.ar/" property="og:url" />
+                    <meta content="website" property="og:type" />
+
+                    <link rel="manifest" href="/manifest.json" />
+                </Head> 
                 <Layout>
                     <Component {...pageProps} />
                 </Layout>
