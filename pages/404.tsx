d650053af01b8533e377df19ac42645d04ac07e7
Mariano Benitez <benitezmarianito@gmail.com>
2022-10-07 18:04:09 -0300

    404 y 500 title agregado

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Divisor from 'components/Divisor'
 import { Footer } from 'components/Layout'
 
 const NotFound: NextPage = () => {
     return (
         <>
+            <Head>
+                <title>Página no encontrada</title>
+            </Head>
             <h2 className='title mt-20 lg:mt-40'>Página no encontrada</h2>
             <Divisor />
             <Footer media map footer />
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Divisor from 'components/Divisor'
 import { Footer } from 'components/Layout'
 
 const ServerError: NextPage = () => {
     return (
         <>
+            <Head>
+                <title>Hubo un error</title>
+            </Head>
             <h2 className='title mt-20 lg:mt-40'>Hubo un error interno</h2>
             <Divisor />
             <Footer media map footer />
